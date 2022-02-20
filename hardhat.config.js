ad94454727b57b17802fe29281c57720ff441de3
Afsha Shaikh <52170654+AFSHA1508@users.noreply.github.com>
2022-02-20 18:45:54 +0530

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,34 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  defaultNetwork: "hardhat",
+  solidity: "0.8.4",
+  paths: {
+    artifacts: './src/artifacts',
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    ropsten: {
+      url: "https://ropsten.infura.io/v3/d37d1eaf679d415f9cef99bcaf7d5881",
+      accounts: [`0x${'7b43617f145ebf15325a7e1f6cef6f5ae2d361b2138f96435caa5b14c02c5a96'}`]
+    }
+  }
+};
